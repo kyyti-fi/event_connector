26fc7fa4f0cc62160988e080726f814ccf87dc9b
Mika Hatakka <Mika.Hatakka@diferion.org>
2016-11-09 06:41:20 +0200

    Hook for taxonomy term alter.

---
+++ event_connector.api.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the Event Connector module.
+ */
+
+/**
+ * Alteration to taxonomy term value send to API.
+ *
+ * @param string $field
+ *    Field name.
+ * @param int $tid
+ *    Term id.
+ * @param string $value
+ *    Value send to API.
+ */
+function hook_event_connector_term_alter($field, $tid, &$value) {
+
+}
